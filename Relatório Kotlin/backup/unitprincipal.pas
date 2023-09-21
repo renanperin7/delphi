1d748d88840f653035b372f7377436ff2675792a
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2023-09-21 18:25:21 -0300

    update

---
+++ Relatório Kotlin/backup/unitprincipal.pas
@@ -0,0 +1,86 @@
+unit unitPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls, fphttpclient;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    pgbProgresso: TProgressBar;
+    SaveDialog1: TSaveDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+     procedure ApplicationException(Sender: TObject; E: Exception);
+     procedure ClientDataReceived(Sender: TObject; const ContentLength,
+      CurrentPos: Int64);
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  URL: String = 'http://localhost:8080/relatorio/vendas?nome=cleber&dataInicial=2020-06-01&dataFinal=2020-06-03';
+  CURL: String = '&dataInicial=';
+  CCURL: String = '&dataFinal=';
+  Client: TFPHTTPClient;
+  FS: TFileStream;
+begin
+  SaveDialog1.FileName := ExtractFileName(URL);
+  if not SaveDialog1.Execute then
+  begin
+    Exit;
+  end;
+
+  Client := TFPHTTPClient.Create(Self);
+  FS     := TFileStream.Create(SaveDialog1.FileName, fmCreate or fmOpenWrite);
+  try
+    Client.OnDataReceived := @ClientDataReceived;
+    Client.Get(URL, FS);
+  finally
+    FS.Free;
+    Client.Free;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+   Application.OnException:=@ApplicationException;
+end;
+
+procedure TForm1.ApplicationException(Sender: TObject; E: Exception);
+begin
+  ShowMessage(E.Message);
+end;
+
+procedure TForm1.ClientDataReceived(Sender: TObject; const ContentLength,
+  CurrentPos: Int64);
+begin
+  pgbProgresso.Max      := ContentLength;
+  pgbProgresso.Position := CurrentPos;
+end;
+
+end.
+
